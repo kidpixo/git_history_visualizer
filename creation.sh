afa07833b2906dd6dcf97308ad10b350a03eaf96
kidpixo <kidpixo@gmail.com>
2015-07-21 12:15:34 +0200

    modified creation.sh

--- creation.sh
+++ creation.sh
@@ -1,3 +1,13 @@
 # initialize repository
 git init
 
+# do stuff with the files
+
+# extrat all the commits 
+git --no-pager log --reverse --oneline
+
+# extract all the filenames
+git --no-pager log --reverse --name-only --oneline --pretty='format:' |  sed '/^$/d' | sort | uniq
+
+# extract the whole git history
+git --no-pager log --reverse --name-status --oneline --pretty='format:COMMIT %h %s' |  tr '\t' ' ' | sed -E -e 's/( )+/ /g' -e '/^$/d'
