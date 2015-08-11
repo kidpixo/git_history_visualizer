b0d539ded85b379d4e3fd4914ec8ff1a8a36aece
kidpixo <kidpixo@gmail.com>
2015-08-11 15:51:44 +0200

    Added decodelog method to set the all_filenames and commits variables after all_commits custom modification. It is calles by get_historytory. Added definedatamatrix method to define a pandas DataFrame with decoded data in the datamatrix attribute. Fixed a bug in datamatrix specification that lost file states.

--- githistoryvis.py
+++ githistoryvis.py
@@ -37,7 +37,7 @@ class git_history:
 
         The method
 
-        foo.get_history()
+        foo.get_history([prettyformat='%h'])
 
         extract the git log, and define:
 
@@ -45,6 +45,12 @@ class git_history:
         - foo.commits     = the commits SHA-1
         - foo.all_files   = all the unique file ever existed
 
+        Prettyformat
+        This is optional, you can define one of the git
+        prettyformat from http://git-scm.com/docs/pretty-formats.
+        I can for example get the whole commit text and write your
+        own parser for sel.decodelog().
+        Deafault is '%h' of the short SHA-1 of the commit.
 
         Status
 
@@ -96,15 +102,56 @@ class git_history:
     def __init__(self, repo_path):
         self.path = repo_path
 
-    def get_history(self):
+    def get_history(self,**kwargs):
+        if 'prettyformat' in kwargs:
+            prettyformat = kwargs['prettyformat']
+        else:
+            prettyformat = "%h"
+
         # get the whole git history
         p = subprocess.check_output(
-            ["git -C "+self.path+""" --no-pager log --reverse --name-status --oneline --pretty='format:C %h' | tr '\t' ' ' | sed -e '/^$/d' """]
+            ["git -C "+self.path+""" --no-pager log --reverse --name-status --oneline --pretty='format:C\t'"""+prettyformat]
             , shell=True, universal_newlines=True)
-        self.all_commits = [i.split(' ') for i in p.split('\n')[0:-1]]
+        # now it is truly pythonic not dependant from machine beneath, as long as git command is available
+        self.all_commits = [i.split('\t') for i in p.split('\n') if '\t' in i]
 
+        self.decodelog()
+
+    def decodelog(self):
         # get all the commits SHA-1
         self.commits = [i[1] for i in self.all_commits if i[0] == 'C']
 
         # get all the file in the history
         self.all_files = sorted(set([i[1] for i in self.all_commits if i[0] != 'C']))
+
+    def definedatamatrix(self):
+        import pandas as pd
+
+        all_filenames = pd.DataFrame(pd.DataFrame(list(self.all_files)),columns=self.commits, index=self.all_files)
+
+        # fill NaN
+        all_filenames.fillna('N', inplace=True)
+
+        actual_commit = previous_commit = 0
+
+        for i in self.all_commits:
+            # set the commit number
+            state,commit_label = i
+            if state == 'C':
+                commit_label_len = len(commit_label)
+                #print '-'*(commit_label_len+5)+'+'+'-'*30
+                #print '>',state,commit_label,
+                # starting at the second commit see which file exist in the previous commit
+                tmp_commit = commit_label
+                if tmp_commit != all_filenames.columns[0]:
+                    previous_commit = actual_commit
+                actual_commit = tmp_commit
+                # assig 1 to file not null un the previous commit
+                if previous_commit != 0:
+                    all_filenames[actual_commit][
+                        (all_filenames[previous_commit] != 'N') & (all_filenames[previous_commit] != 'D')] = 'S'
+                #print  "| previous %s : actual %s" % (previous_commit,actual_commit)
+            else:
+                all_filenames.ix[commit_label,actual_commit] = state
+                #print ' '*(commit_label_len+4),'|',state,commit_label
+        self.datamatrix = all_filenames
