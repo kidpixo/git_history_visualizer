79866e2b829a395c8c16b763eb917789cd978f02
kidpixo <kidpixo@gmail.com>
2016-07-20 15:11:11 +0200

    updated example and notebook, added a sample git lor output in gitoutput file, added some gitignore (vim, ipython notebook)

--- example_githistoryvis.py
+++ example_githistoryvis.py
@@ -3,18 +3,31 @@ import githistoryvis as ghv
 
 # put here the desired git repo path
 import os
-path =  os.getcwd()
+path = os.getcwd()
 
 # define the obejct linked to your repository
+# essentially add the gt.path variable
 gt = ghv.git_history(path)
-# get the history
+# get the history : define gt.all_commits, gt.commit, gt.all_file
 gt.get_history()
-# define the datamatrix
+
+# if the git log data are in a file somewhere,
+# read the file in a string and pass it
+with open('gitoutput', 'r') as file:
+    data = file.read()
+gt.get_history(gitcommitlist=data)
+
+# Here is Pandas needed
+# define the datamatrix : define gt.datamatrix,
+# a Pandas.DataFrame with categorical columns
 gt.definedatamatrix()
+
+# new compact version
+gt = ghv.git_history(path, get_history=False, definedatamatrix=False)
 
 # visualization
 import matplotlib
 from matplotlib import pyplot as plt
 
 # play wiht size and figsize to find yours
-gt.plot_history_df(plt,gt.datamatrix,size= 300, figsize = [12,10.5],outpath=path+os.sep+'images/complete_visual_history.png')
+gt.plot_history_df(plt, gt.datamatrix, size=300, figsize=[12, 10.5], outpath=path+os.sep+'images/complete_visual_history.png')
